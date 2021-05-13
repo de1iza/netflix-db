54a4409734e4863c9a0c3e53aa518789cda2d8fc
Demina Elizaveta <liza_demina11@mail.ru>
2021-05-14 00:25:23 +0300

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,69 +1,74 @@
-create table tv_show(
+create schema shows;
+
+create table shows.tv_show(
     show_id serial primary key,
     title text not null,
-    genre text,
-    release_date date,
+    genres text array,
+    release_year integer,
+    check (release_year between 1900 and 2021),
     n_seasons integer,
     n_episodes integer);
 
-create table director(
+create table shows.director(
     director_id serial primary key,
     first_name text not null,
     last_name text);
 
-create table episode(
+create table shows.episode(
     episode_id serial primary key,
-    show_id serial references tv_show(show_id),
+    show_id serial references shows.tv_show(show_id),
     title text,
     number_in_season integer not null,
     season integer not null
 );
 
-create table statistics(
-    show_id serial primary key references tv_show(show_id),
+create table shows.statistics(
+    show_id serial primary key references shows.tv_show(show_id),
     imdb_rating float,
     check (imdb_rating between 1 and 10),
     rt_rating float,
     check (rt_rating between 0 and 100)
 );
 
-create table country(
+create table shows.country(
     country_name text primary key
 );
 
-create table company(
+create table shows.company(
     company_id serial primary key,
     company_name text,
-    country_name text references country(country_name)
+    country_name text references shows.country(country_name)
 );
 
-create table actor(
+create table shows.actor(
     actor_id serial primary key,
     first_name text not null,
     last_name text,
-    country_name text references country(country_name)
+    country_name text references shows.country(country_name)
 );
 
-alter table tv_show add country_name text references country(country_name);
+alter table shows.tv_show add country_name text references shows.country(country_name);
 
-alter table director add country_name text references country(country_name);
+alter table shows.director add country_name text references shows.country(country_name);
 
-create table show_cast(
+create table shows.show_cast(
     id serial primary key,
-    show_id serial references tv_show(show_id),
-    actor_id serial references actor(actor_id)
+    show_id serial references shows.tv_show(show_id),
+    actor_id serial references shows.actor(actor_id)
 );
 
-create table show_companies(
+create table shows.show_companies(
     id serial primary key,
-    show_id serial references tv_show(show_id),
-    company_id serial references company(company_id)
+    show_id serial references shows.tv_show(show_id),
+    company_id serial references shows.company(company_id)
 );
 
-alter table show_companies rename to production_companies;
+alter table shows.show_companies rename to production_companies;
 
-create table show_directors(
+create table shows.show_directors(
     id serial primary key,
-    show_id serial references tv_show(show_id),
-    director_id serial references director(director_id)
+    show_id serial references shows.tv_show(show_id),
+    director_id serial references shows.director(director_id)
 );
+
+
